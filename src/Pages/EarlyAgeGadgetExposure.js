213e4bcb0fb8136ca371dfde4d0ef7d3f4e5ecbc
aleeshyakrishna <aleeshyakrishnamv@gmail.com>
2024-03-06 17:15:28 +0530

    early age gadget exposure box

--- src/Pages/EarlyAgeGadgetExposure.js
+++ src/Pages/EarlyAgeGadgetExposure.js
@@ -10,6 +10,7 @@ import Roundb from "../Images/Roundb.png";
 import earlyAgeBoxImg from "../Images/earlyAgeBoxImg.png";
 import '../Css/EarlyAgeGadgetExposure.css'
 import Group119 from '../Images/Group 119.png'
+import Maskgroup from '../Images/Mask group.png'
 const EarlyAgeGadgetExposure = () => {
   return (
     <div className="home-main">
@@ -72,8 +73,65 @@ const EarlyAgeGadgetExposure = () => {
                 <h1>Gadgts on</h1>
                 <p>children</p> */}
               </div>
-                
+
               <img src={earlyAgeBoxImg} alt="" />
+            </div>
+          </div>
+        </div>
+
+        <div className="goalvision-headrer-box">
+          
+          <div className="goal-box">
+            <div className="inside-box">
+              <h1>Get to know How Much You are Attracted to Gadgets</h1>
+              {/* <img src={Yellowline} alt="" /> */}
+              {/* <p>
+                Lorem ipsum dolor sit amet doloroli sitiol conse ctetur
+                adipiscing elit.{" "}
+              </p> */}
+               <div className="buttonss">
+          {/* <p data-aos="zoom-in" data-aos-duration="1000">
+            We believe that early age gadget exposure, adaptaion to mobile among
+            children has now led to a time where there decrease in learning
+          </p> */}
+          <button>Take Test</button>
+        </div>
+            </div>
+            <div>
+              <img
+                src={Maskgroup}
+                style={{
+                  // backgroundColor: "red",
+                  zIndex: 1,
+                  marginTop: 100,
+                  marginLeft: -250,
+                  width: 330,
+                }}
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="goal-box">
+            <div className="inside-box">
+              <h1>Our Goals</h1>
+              {/* <img src={Yellowline} alt="" /> */}
+              {/* <p>
+                Lorem ipsum dolor sit amet doloroli sitiol conse ctetur
+                adipiscing elit.{" "}
+              </p> */}
+            </div>
+            <div>
+              <img
+                // src={Flag}
+                style={{
+                  // backgroundColor: "red",
+                  zIndex: 1,
+                  marginTop: 100,
+                  width: 330,
+                  marginLeft: -240,
+                }}
+                alt=""
+              />
             </div>
           </div>
         </div>
