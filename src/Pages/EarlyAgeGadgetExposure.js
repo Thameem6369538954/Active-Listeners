a2d06366cbe627bf9302d3992e4f8fcc99068988
thameemansari <thameemrpp@gmail.com>
2024-03-07 16:17:05 +0530

    new ubdate

--- src/Pages/EarlyAgeGadgetExposure.js
+++ src/Pages/EarlyAgeGadgetExposure.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
-import GetinTouch from '../Components/GetinTouch'
-import Footer from '../Components/Footer'
-import Therapycategories from '../Components/Therapycategories'
-import earlyAgeright from "../Images/Earlyageright2.png"
-import earlyAgeleft from '../Images/earlyAgeleft.png'
+import React from "react";
+import Navbar from "../Components/Navbar";
+import GetinTouch from "../Components/GetinTouch";
+import Footer from "../Components/Footer";
+import Therapycategories from "../Components/Therapycategories";
+import earlyAgeright2 from "../Images/earlyAgeright.png";
+import earlyAgeleft from "../Images/earlyAgeleft.png";
 import Rounda from "../Images/Rounda.png";
 import Roundb from "../Images/Roundb.png";
 import earlyAgeBoxImg from "../Images/earlyAgeBoxImg.png";
-import '../Css/EarlyAgeGadgetExposure.css'
-import Group119 from '../Images/Group 119.png'
-import Maskgroup from '../Images/Mask group.png'
+import "../Css/EarlyAgeGadgetExposure.css";
+import Group119 from "../Images/Group 119.png";
+import Maskgroup from "../Images/Mask group.png";
+import cover from "../Images/cover.png";
+import PodcastRed from "../Images/PodcastRed.png";
 const EarlyAgeGadgetExposure = () => {
   return (
     <div className="home-main">
@@ -35,7 +37,7 @@ const EarlyAgeGadgetExposure = () => {
             </h3>
           </div>
           <img
-            src={earlyAgeright}
+            src={earlyAgeright2}
             data-aos="fade-right"
             data-aos-duration="1000"
             alt=""
@@ -48,7 +50,7 @@ const EarlyAgeGadgetExposure = () => {
           </p> */}
           <button>Talk to us</button>
         </div>
-        
+
         {/* <div className="hdr-btm-text">
           <p data-aos="zoom-in" data-aos-duration="1000">
             We believe that early age gadget exposure, adaptaion to mobile among
@@ -57,90 +59,55 @@ const EarlyAgeGadgetExposure = () => {
           <button>Why Active Listeners?</button>
         </div> */}
       </div>
-      <div className="green-bourd-grief">
-          <div class="card-grif">
-            <div class="circle">
-              <img src={Roundb} alt="" />
-            </div>
-            <div class="circle-b">
-              <img src={Rounda} alt="" />
-            </div>
+      <div className="green-bourd-Earlyage">
+        <div class="card-grif">
+          <div class="circle">
+            <img src={Roundb} alt="" />
+          </div>
+          <div class="circle-b">
+            <img src={Rounda} alt="" />
+          </div>
 
-            <div class="card-inneR">
-              <div>
-                
-                {/* <h1>Impact of</h1>
-                <h1>Gadgts on</h1>
-                <p>children</p> */}
-              </div>
-
-              <img src={earlyAgeBoxImg} alt="" />
+          <div class="card-inneR">
+            <div className="card-inneR-text">
+              <h1>Impact of</h1>
+              <h1>Gadgts on</h1>
+              <p>children</p>
             </div>
+            <img src={earlyAgeBoxImg} alt="" />
           </div>
         </div>
-
-        <div className="goalvision-headrer-box">
-          
-          <div className="goal-box">
-            <div className="inside-box">
-              <h1>Get to know How Much You are Attracted to Gadgets</h1>
-              {/* <img src={Yellowline} alt="" /> */}
-              {/* <p>
-                Lorem ipsum dolor sit amet doloroli sitiol conse ctetur
-                adipiscing elit.{" "}
-              </p> */}
-               <div className="buttonss">
-          {/* <p data-aos="zoom-in" data-aos-duration="1000">
-            We believe that early age gadget exposure, adaptaion to mobile among
-            children has now led to a time where there decrease in learning
-          </p> */}
-          <button>Take Test</button>
-        </div>
-            </div>
-            <div>
-              <img
-                src={Maskgroup}
-                style={{
-                  // backgroundColor: "red",
-                  zIndex: 1,
-                  marginTop: 100,
-                  marginLeft: -250,
-                  width: 330,
-                }}
-                alt=""
-              />
-            </div>
+      </div>
+      <div className="getyoukow-main-container">
+        <div className="getyoukow-container">
+          <div className="getyouknow-txt">
+            <h1>Get to know How much you are Attracted to Gadgets</h1>
+            <button>Take Rest </button>
           </div>
-          <div className="goal-box">
-            <div className="inside-box">
-              <h1>Our Goals</h1>
-              {/* <img src={Yellowline} alt="" /> */}
-              {/* <p>
-                Lorem ipsum dolor sit amet doloroli sitiol conse ctetur
-                adipiscing elit.{" "}
-              </p> */}
-            </div>
-            <div>
-              <img
-                // src={Flag}
-                style={{
-                  // backgroundColor: "red",
-                  zIndex: 1,
-                  marginTop: 100,
-                  width: 330,
-                  marginLeft: -240,
-                }}
-                alt=""
-              />
-            </div>
+          <div className="getyouknow-image">
+            <img src={Maskgroup} alt="" />
           </div>
         </div>
-      <Therapycategories/>
+        <div className="getyoukow-container">
+          <div className="getyouknow-image">
+            <img
+              src={cover}
+              style={{ width: "80%", marginLeft: "10%", borderRadius: "20px" }}
+              alt=""
+            />
+          </div>
+          <div className="getyouknow-txt">
+            <h1>Get to know How much you are Attracted to Gadgets</h1>
+            <button>Take Rest </button>
+          </div>
+          <img src={PodcastRed} className="getyoukow-image-red" alt="" />
+        </div>
+      </div>
+      <Therapycategories />
       <GetinTouch />
       <Footer />
     </div>
-    
-  )
-}
+  );
+};
 
-export default EarlyAgeGadgetExposure
+export default EarlyAgeGadgetExposure;
